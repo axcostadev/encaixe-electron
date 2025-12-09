7e44ea1c3c1aa92300de1ab4187c11226a169a8a
Alex Costa Dev <227930068+axcostadev@users.noreply.github.com>
2025-12-09 09:01:01 -0300

    feat: integrar electron

--- electron-app/main.cjs
+++
@@ -1,9 +0,0 @@
-// Bootstrapper to allow Electron (CommonJS) to load an ES module main.js
-(async () => {
-  try {
-    await import('./main.js');
-  } catch (err) {
-    console.error('Failed to import main.js:', err);
-    process.exit(1);
-  }
-})();
--- electron-app/renderer/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import TelaEncaixe from './components/TelaEncaixe'
-
-export default function App() {
-  return <TelaEncaixe />
-}
